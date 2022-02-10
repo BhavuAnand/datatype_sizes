63c63ca889fae06113967434079959a62850a71d
BhavuAnand <91019009+BhavuAnand@users.noreply.github.com>
2022-02-10 20:11:37 +0530

    Update sizes.cpp

--- sizes.cpp
+++ sizes.cpp
@@ -1,3 +1,4 @@
+//size of datatypes
 #include <iostream>
 using namespace std;
 
@@ -7,6 +8,25 @@ int main()
     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
     cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
+
+
+//product of two numbers
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    float n1,n2,pr;
+    cout << "Enter the numbers for product"<<endl;
+    cout << "Enter the first number: ";
+    cin>>n1;
+    cout << "Enter the second number: ";
+    cin>>n2;
+    pr=n1*n2;
+    cout<<"Product is: "<<pr;
 
     return 0;
 }
